bb71fd029bb8ed7ae018830d66fc15d684ecf089
fckgm <79022002+fckgm@users.noreply.github.com>
2021-08-03 10:03:34 +0300

    Подготовка к релизу 1.1

--- errors.py
+++
@@ -1,8 +0,0 @@
-class ClientException(Exception):
-    """ Базовое исключение """
-    pass
-
-
-class RestartPlinkCountException(ClientException):
-    """ Вызвыается, если было превышено кол-во допустимых перезапусков plink-а """
-    pass
