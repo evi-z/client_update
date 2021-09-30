294bd387ca5e48c38c9cf07063ce42c5ac1af833
fckgm <79022002+fckgm@users.noreply.github.com>
2021-09-30 16:29:24 +0300

    Очистка репозитория перед 1.1.5

--- errors.py
+++
@@ -1,8 +0,0 @@
-class ClientException(Exception):
-    """ Базовое исключение """
-    pass
-
-
-class ReconnectForRewriteDBException(ClientException):
-    """Вызывается, если от сервера получен ответ об отсутсвии записи в БД"""
-    pass
